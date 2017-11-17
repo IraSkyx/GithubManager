4a0ee2b5f3d2fe91830f432dbade3b3de8c97d6a
Adrien Lenoir <adrianou77@live.fr>
2017-11-17 09:54:56 +0100

    Work
    - Code clean up
    - Modified class diagram, need to go further and add features ! (I'm out of idea)
    - FrontController is now full ok, validated by the professor

---
+++ Application/src/controller/FrontController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Adrien
+ */
+public class FrontController {
+    private static Stage stage;
+    
+    public static void setStage(Stage primaryStage, BorderPane panel){
+        stage = primaryStage;
+        stage.setWidth(1080);
+        stage.setHeight(720);
+        stage.setTitle("GithubManager");
+        setScene(panel);
+        stage.show();
+    }
+    
+    public static void setScene(BorderPane panel){
+        Scene scene = new Scene(panel, stage.getHeight(), stage.getWidth());           
+        scene.getStylesheets().add(FrontController.class.getResource("/ihm/application.css").toExternalForm());
+        stage.setScene(scene);
+    }
+}
--- Application/src/launch/Main.java
+++ Application/src/launch/Main.java
@@ -1,36 +1,20 @@
 package launch;
 
+import controller.FrontController;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
-
-    private static Stage primaryStage;
-    
-    private void setPrimaryStage(Stage stage) {
-        Main.primaryStage = stage;
-    }
-    
-    static public Stage getPrimaryStage() {
-        return Main.primaryStage;
-    }
         
     @Override
     public void start(Stage stage) {
-        try {
-            BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("/ihm/Home.fxml"));
-            Scene scene = new Scene(root, 1080, 720);           
-            scene.getStylesheets().add(getClass().getResource("/ihm/application.css").toExternalForm());
-            setPrimaryStage(stage);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("GitHubManager");
-            primaryStage.show();
+        try {       
+            FrontController.setStage(stage, (BorderPane) FXMLLoader.load(getClass().getResource("/ihm/Home.fxml")));
         } catch (IOException e) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, e);
         }
