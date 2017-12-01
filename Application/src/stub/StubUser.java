5700625bb2fd9de06c0f5109ff321bf28ab83255
IraSkyx <planete.skyx@gmail.com>
2017-12-01 13:26:20 +0100

    Added Stub for Users and Follows

--- Application/src/stub/StubUser.java
+++ Application/src/stub/StubUser.java
@@ -5,8 +5,12 @@
  */
 package stub;
 
+import business_logic.APIGateway;
+import business_logic.Category;
+import business_logic.Repository;
+import business_logic.RepositoryMaker;
 import business_logic.User;
-import java.lang.reflect.Array;
+import business_logic.UsersManager;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -16,9 +20,28 @@ import java.util.Arrays;
  */
 public class StubUser {
     public static ArrayList<User> loadUsers(){
-        return Arrays.(
+        ArrayList<User> allUsers = new ArrayList<>(Arrays.asList(
             new User("Adrien", "Lenoir", "adrien.lenoir@etu.uca.fr"),
             new User("Fabien", "Simonet", "fabien.simonet@etu.uca.fr")    
-        );
+        ));
+        loadFollows(allUsers);
+        return allUsers;
+    }
+    
+    public static void loadFollows(ArrayList<User> allUsers){
+        for(User user : allUsers){
+            Category categ = new Category("Future projects");
+            for(Repository repo : APIGateway.getRepositories("IraSkyx"))
+                categ.AddFollow(repo);
+            user.getFollows().AddFollow(categ);
+            
+            Category categ2 = new Category("TODOLIST");
+            for(Repository repo : APIGateway.getRepositories("ElRaffray"))
+                categ2.AddFollow(repo);
+            user.getFollows().AddFollow(categ2);
+            
+            for(Repository repo : APIGateway.getRepositories("GabinSalabert"))
+                user.getFollows().AddFollow(repo);
+        }
     }
 }
