a3b54aa5ac4e24bf097eb566813ca6b96df222f2
fabien.simonet@etu.uca.fr <fabien.simonet@etu.uca.fr>
2017-12-17 12:25:06 +0100

    Use case diagram

--- Application/src/business_logic/persistance/xml/XMLUsers.java
+++ Application/src/business_logic/persistance/xml/XMLUsers.java
@@ -16,44 +16,7 @@ import java.util.ArrayList;
  *
  * @author fasimonet
  */
-public class XMLUsers implements DataManager, Serializable {
+public class XMLUsers implements Serializable {
 
-    /**
-     * Load users using XML deserialization
-     * @return Every loaded users
-     */
-    
-    private APIManager apiManager;
-    
-    public XMLUsers(APIManager apiManager){
-        this.apiManager = apiManager;
-    }
-    
-    public void setAPIManager(APIManager apiManager){
-        this.apiManager = apiManager;
-    }
-    
-    @Override
-    public ArrayList<IUser> loadUsers() {
-        ArrayList<IUser> result = null;
-        try (XMLDecoder ois = new XMLDecoder(new FileInputStream("users.xml"))) {
-            result = (ArrayList<IUser>)ois.readObject();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-    
-    /**
-     * Save users using XML serialization
-     * @param users All the users to serialize
-     */
-    @Override
-    public void saveUsers(ArrayList<IUser> users) {
-        try (XMLEncoder oos = new XMLEncoder(new FileOutputStream("users.xml"))) {
-            oos.writeObject(UsersManager.getAllUsers());        
-        }catch (IOException e) {
-            e.printStackTrace();
-        }      
-    }
+
 }
