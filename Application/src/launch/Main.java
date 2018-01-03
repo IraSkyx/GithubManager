d0f248c9481b970586200882a4d50377ddeee214
fabien.simonet@etu.uca.fr <fabien.simonet@etu.uca.fr>
2018-01-03 15:23:18 +0100

    code cleaned

--- Application/src/launch/Main.java
+++ Application/src/launch/Main.java
@@ -5,8 +5,6 @@ import business_logic.persistance.stub.StubUsers;
 import business_logic.persistance.xml.XMLDataManager;
 import business_logic.user.UsersManager;
 import controller.FrontController;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
@@ -16,19 +14,12 @@ public class Main extends Application {
         
     @Override
     public void start(Stage stage) {
-        UsersManager.setDataManager(new XMLDataManager());
-        //UsersManager.setDataManager(new StubUsers(new GitHubGateway()));           
+        UsersManager.setDataManager(new XMLDataManager());         
         UsersManager.loadUsers();     
 
         Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
-        //GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-        //stage.setWidth(gd.getDisplayMode().getWidth()*0.6);
-        //stage.setHeight(gd.getDisplayMode().getHeight()*0.6);
         stage.setWidth(visualBounds.getWidth());
         stage.setHeight(visualBounds.getHeight());
-        
-        System.out.println(visualBounds.getWidth());
-        System.out.println(visualBounds.getHeight());
         stage.centerOnScreen();
         stage.setTitle("GithubManager");
         FrontController.setStage(stage);
